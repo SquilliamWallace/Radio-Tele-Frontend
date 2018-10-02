51375e1e415b64a45e0d01342309edfa30f0f0b5
Derek McClellan <dmcclellan@ycp.edu>
2018-10-02 10:12:46 -0400

    Fix to bundler and routing

--- vue-front-end/src/views/Home.vue
+++ vue-front-end/src/views/Home.vue
@@ -1,5 +1,14 @@
 <template>
-    <div>this is a test</div>
+    <v-toolbar>
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-title>Title</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn flat>Link One</v-btn>
+        <v-btn flat>Link Two</v-btn>
+        <v-btn flat>Link Three</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
 </template>
 
 <script>
