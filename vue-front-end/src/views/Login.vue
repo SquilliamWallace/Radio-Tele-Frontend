988d28cd90b046495fec7bc5a33c1dbe645c28a0
Joey Hopkins <jhopkins1@ycp.edu>
2018-10-02 10:19:04 -0400

    Add basic implementation of login page and fix dependencies

---
+++ vue-front-end/src/views/Login.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id = "login"> 
+        <input type="text" name="username" v-model="input.username" placeholder="Username" />
+        <input type="password" name="password" v-model="input.password" placeholder="Password" />
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Login",
+    data() {
+        return {
+            input: {
+                username: "",
+                password: ""
+            }
+        }
+    },
+    methods: {
+        login(){
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+    #login {
+        width: 400px;
+    }
+</style>
