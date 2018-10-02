fd42956057f4210f07dffa4d4c101e7c22532063
Derek McClellan <dmcclellan@ycp.edu>
2018-10-02 19:10:29 -0400

    Implement basic home page

--- vue-front-end/src/views/Home.vue
+++ vue-front-end/src/views/Home.vue
@@ -1,14 +1,62 @@
 <template>
+<div>
     <v-toolbar>
-      <v-toolbar-side-icon></v-toolbar-side-icon>
+          <v-menu
+            v-model="menuShow"
+            :close-on-content-click="false"
+            :nudge-width="200"
+            offset-x>
+            <v-toolbar-side-icon slot="activator"></v-toolbar-side-icon>
+            <v-card>
+            <v-list>
+                <v-list-tile avatar>
+                <v-list-tile-avatar>
+                    <img src="https://i.kym-cdn.com/entries/icons/original/000/009/803/spongebob-squarepants-patrick-spongebob-patrick-star-background-225039.jpg" alt="Patrick">
+                </v-list-tile-avatar>
+    
+                <v-list-tile-content>
+                    <v-list-tile-title>Patrick Star</v-list-tile-title>
+                    <v-list-tile-sub-title>Founder of the Big Rock Club</v-list-tile-sub-title>
+                </v-list-tile-content>
+                </v-list-tile>
+
+                <v-spacer></v-spacer>
+                <v-subheader inset>Navigation</v-subheader>
+                <v-divider inset></v-divider>
+
+                <v-list-tile-content style="padding-left: 10px;">
+                    <v-list-tile-title>Link 1</v-list-tile-title>
+                    <v-list-tile-sub-title>This will be a link</v-list-tile-sub-title>
+                </v-list-tile-content>
+
+                <v-list-tile-content style="padding-left: 10px;">
+                    <v-list-tile-title>Link 2</v-list-tile-title>
+                    <v-list-tile-sub-title>This, too, will be a link</v-list-tile-sub-title>
+                </v-list-tile-content>
+
+                <v-list-tile-content style="padding-left: 10px;">
+                    <v-list-tile-title>Link 3</v-list-tile-title>
+                    <v-list-tile-sub-title>And this also</v-list-tile-sub-title>
+                </v-list-tile-content>
+            </v-list>
+  
+          <v-divider></v-divider>
+  
+          <v-card-actions>
+            <v-spacer></v-spacer>
+          </v-card-actions>
+        </v-card>
+        </v-menu>
       <v-toolbar-title>YCAS Radio Telescope</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn flat>Link One</v-btn>
-        <v-btn flat>Link Two</v-btn>
-        <v-btn flat>Link Three</v-btn>
+        <v-btn to="/login" flat>Login</v-btn>
+        <v-btn to="/register">Register</v-btn>
+        <v-btn to="/profile">Profile</v-btn>
       </v-toolbar-items>
     </v-toolbar>
+    <v-parallax style="height:100%;" src="https://images.pexels.com/photos/110854/pexels-photo-110854.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></v-parallax>
+</div>
 </template>
 
 <script>
@@ -16,8 +64,10 @@ export default {
     name: "Home",
     data() {
         return {
-
+            menuShow: false
         }
+    },
+    methods: {
     }
 }
 </script>
