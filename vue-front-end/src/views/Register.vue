9be1372d662d32000474a6771cec753d14289a22
mhasz239 <mhasz239@gmail.com>
2018-10-02 23:39:58 -0400

    added generic shopify terms and conditions into form

--- vue-front-end/src/views/Register.vue
+++ vue-front-end/src/views/Register.vue
@@ -99,8 +99,8 @@
                 <v-dialog v-model="terms" width="70%">
                 <v-card>
                     <v-card-title class="title">Terms</v-card-title>
-                    <v-card-text v-for="n in 5" :key="n">
-                    {{ content }}
+                    <v-card-text>
+                    {{ contentTerms }}
                     </v-card-text>
                     <v-card-actions>
                     <v-spacer></v-spacer>
@@ -115,8 +115,8 @@
                 <v-dialog v-model="conditions" width="70%">
                 <v-card>
                     <v-card-title class="title">Conditions</v-card-title>
-                    <v-card-text v-for="n in 5" :key="n">
-                    {{ content }}
+                    <v-card-text>
+                    {{ contentConditions }}
                     </v-card-text>
                     <v-card-actions>
                     <v-spacer></v-spacer>
@@ -163,6 +163,8 @@
         snackbar: false,
         terms: false,
         phoneMask: 'phone',
+        contentTerms: 'TERMS OF SERVICE\n\n----\n\nOVERVIEW\nThis website is operated by YCAS. Throughout the site, the terms “we”, “us” and “our” refer to YCAS. YCAS offers this website, including all information, tools and services available from this site to you, the user, conditioned upon your acceptance of all terms, conditions, policies and notices stated here.\nBy visiting our site and/ or purchasing something from us, you engage in our “Service” and agree to be bound by the following terms and conditions (“Terms of Service”, “Terms”), including those additional terms and conditions and policies referenced herein and/or available by hyperlink. These Terms of Service apply  to all users of the site, including without limitation users who are browsers, vendors, customers, merchants, and/ or contributors of content.\nPlease read these Terms of Service carefully before accessing or using our website. By accessing or using any part of the site, you agree to be bound by these Terms of Service. If you do not agree to all the terms and conditions of this agreement, then you may not access the website or use any services. If these Terms of Service are considered an offer, acceptance is expressly limited to these Terms of Service.\nAny new features or tools which are added to the current store shall also be subject to the Terms of Service. You can review the most current version of the Terms of Service at any time on this page. We reserve the right to update, change or replace any part of these Terms of Service by posting updates and/or changes to our website. It is your responsibility to check this page periodically for changes. Your continued use of or access to the website following the posting of any changes constitutes acceptance of those changes.\nOur store is hosted on Shopify Inc. They provide us with the online e-commerce platform that allows us to sell our products and services to you.',
+        contentConditions: 'TERMS OF CONDITIONS\n\n----\n\nOVERVIEW\nThis website is operated by YCAS. Throughout the site, the terms “we”, “us” and “our” refer to YCAS. YCAS offers this website, including all information, tools and services available from this site to you, the user, conditioned upon your acceptance of all terms, conditions, policies and notices stated here.\nBy visiting our site and/ or purchasing something from us, you engage in our “Service” and agree to be bound by the following terms and conditions (“Terms of Service”, “Terms”), including those additional terms and conditions and policies referenced herein and/or available by hyperlink. These Terms of Service apply  to all users of the site, including without limitation users who are browsers, vendors, customers, merchants, and/ or contributors of content.\nPlease read these Terms of Service carefully before accessing or using our website. By accessing or using any part of the site, you agree to be bound by these Terms of Service. If you do not agree to all the terms and conditions of this agreement, then you may not access the website or use any services. If these Terms of Service are considered an offer, acceptance is expressly limited to these Terms of Service.\nAny new features or tools which are added to the current store shall also be subject to the Terms of Service. You can review the most current version of the Terms of Service at any time on this page. We reserve the right to update, change or replace any part of these Terms of Service by posting updates and/or changes to our website. It is your responsibility to check this page periodically for changes. Your continued use of or access to the website following the posting of any changes constitutes acceptance of those changes.\nOur store is hosted on Shopify Inc. They provide us with the online e-commerce platform that allows us to sell our products and services to you.',
         defaultForm
       }
     },
