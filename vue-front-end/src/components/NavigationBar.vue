ee427dda95a75fa29bf7053e1151d3df7fa46afa
Derek McClellan <dmcclellan@ycp.edu>
2018-10-09 10:29:43 -0400

    Separate navigation bar into separate component

---
+++ vue-front-end/src/components/NavigationBar.vue
@@ -0,0 +1,67 @@
+<template>
+    <v-toolbar style="width:100%;">
+          <v-menu
+            v-model="menuShow"
+            :close-on-content-click="false"
+            :nudge-width="200"
+            offset-x>
+            <v-toolbar-side-icon slot="activator"></v-toolbar-side-icon>
+            <v-card>
+            <v-list>
+                <v-list-tile avatar>
+                <v-list-tile-avatar>
+                    <img src="https://i.kym-cdn.com/entries/icons/original/000/009/803/spongebob-squarepants-patrick-spongebob-patrick-star-background-225039.jpg" alt="Patrick">
+                </v-list-tile-avatar>
+    
+                <v-list-tile-content>
+                    <v-list-tile-title>Patrick Star</v-list-tile-title>
+                    <v-list-tile-sub-title>Founder of the Big Rock Club</v-list-tile-sub-title>
+                </v-list-tile-content>
+                </v-list-tile>
+
+                <v-spacer></v-spacer>
+                <v-subheader inset>Navigation</v-subheader>
+                <v-divider inset></v-divider>
+
+                <v-list-tile-content style="padding-left: 10px;">
+                    <v-list-tile-title>Link 1</v-list-tile-title>
+                    <v-list-tile-sub-title>This will be a link</v-list-tile-sub-title>
+                </v-list-tile-content>
+
+                <v-list-tile-content style="padding-left: 10px;">
+                    <v-list-tile-title>Link 2</v-list-tile-title>
+                    <v-list-tile-sub-title>This, too, will be a link</v-list-tile-sub-title>
+                </v-list-tile-content>
+
+                <v-list-tile-content style="padding-left: 10px;">
+                    <v-list-tile-title>Link 3</v-list-tile-title>
+                    <v-list-tile-sub-title>And this also</v-list-tile-sub-title>
+                </v-list-tile-content>
+            </v-list>
+  
+          <v-divider></v-divider>
+  
+          <v-card-actions>
+            <v-spacer></v-spacer>
+          </v-card-actions>
+        </v-card>
+        </v-menu>
+      <v-toolbar-title>YCAS Radio Telescope</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn to="/login" flat>Login</v-btn>
+        <v-btn to="/register">Register</v-btn>
+        <v-btn to="/profile">Profile</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+</template>
+
+<script>
+export default {
+    name: 'Navigation Bar'
+}
+</script>
+
+<style scoped>
+
+</style>
--- vue-front-end/src/views/Home.vue
+++ vue-front-end/src/views/Home.vue
@@ -1,61 +1,7 @@
 <template>
 <div>
     <v-parallax style = "height:100%;" src="https://images.pexels.com/photos/110854/pexels-photo-110854.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260">
-    <v-toolbar style="width:100%;">
-          <v-menu
-            v-model="menuShow"
-            :close-on-content-click="false"
-            :nudge-width="200"
-            offset-x>
-            <v-toolbar-side-icon slot="activator"></v-toolbar-side-icon>
-            <v-card>
-            <v-list>
-                <v-list-tile avatar>
-                <v-list-tile-avatar>
-                    <img src="https://i.kym-cdn.com/entries/icons/original/000/009/803/spongebob-squarepants-patrick-spongebob-patrick-star-background-225039.jpg" alt="Patrick">
-                </v-list-tile-avatar>
-    
-                <v-list-tile-content>
-                    <v-list-tile-title>Patrick Star</v-list-tile-title>
-                    <v-list-tile-sub-title>Founder of the Big Rock Club</v-list-tile-sub-title>
-                </v-list-tile-content>
-                </v-list-tile>
-
-                <v-spacer></v-spacer>
-                <v-subheader inset>Navigation</v-subheader>
-                <v-divider inset></v-divider>
-
-                <v-list-tile-content style="padding-left: 10px;">
-                    <v-list-tile-title>Link 1</v-list-tile-title>
-                    <v-list-tile-sub-title>This will be a link</v-list-tile-sub-title>
-                </v-list-tile-content>
-
-                <v-list-tile-content style="padding-left: 10px;">
-                    <v-list-tile-title>Link 2</v-list-tile-title>
-                    <v-list-tile-sub-title>This, too, will be a link</v-list-tile-sub-title>
-                </v-list-tile-content>
-
-                <v-list-tile-content style="padding-left: 10px;">
-                    <v-list-tile-title>Link 3</v-list-tile-title>
-                    <v-list-tile-sub-title>And this also</v-list-tile-sub-title>
-                </v-list-tile-content>
-            </v-list>
-  
-          <v-divider></v-divider>
-  
-          <v-card-actions>
-            <v-spacer></v-spacer>
-          </v-card-actions>
-        </v-card>
-        </v-menu>
-      <v-toolbar-title>YCAS Radio Telescope</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn to="/login" flat>Login</v-btn>
-        <v-btn to="/register">Register</v-btn>
-        <v-btn to="/profile">Profile</v-btn>
-      </v-toolbar-items>
-    </v-toolbar>
+    <navigation-bar></navigation-bar>
     <v-spacer></v-spacer>
     <v-card>
           <v-img
@@ -89,6 +35,7 @@
 </template>
 
 <script>
+import NavigationBar from '../components/NavigationBar.vue'
 export default {
     name: "Home",
     data() {
@@ -96,6 +43,9 @@ export default {
             menuShow: false,
             show: false
         }
+    },
+    components: {
+      NavigationBar
     },
     methods: {
     }
