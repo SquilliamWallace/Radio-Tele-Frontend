59c511e7783f4a34ea7fc069e4045c49f7e987b9
Joey Hopkins <jhopkins1@ycp.edu>
2018-10-09 15:46:48 -0400

    Implemented appointment view page

--- vue-front-end/src/router.js
+++ vue-front-end/src/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Login from './views/Login.vue'
 import Register from './views/Register.vue'
+import AppointmentView from './views/AppointmentView.vue'
 
 Vue.use(Router)
 
@@ -24,6 +25,11 @@ const router = new Router({
       path: '/register',
       name: 'register',
       component: Register
+    },
+    {
+      path: '/appointmentView',
+      name: 'appointmentView',
+      component: AppointmentView
     }
   ]
 })
---
+++ vue-front-end/src/views/AppointmentView.vue
@@ -0,0 +1,156 @@
+<template>
+    <div>
+        <v-toolbar style="width:100%;">
+          <v-menu
+            v-model="menuShow"
+            :close-on-content-click="false"
+            :nudge-width="200"
+            offset-x>
+            <v-toolbar-side-icon slot="activator"></v-toolbar-side-icon>
+            <v-card>
+            <v-list>
+                <v-list-tile avatar>
+                <v-list-tile-avatar>
+                    <img src="https://i.kym-cdn.com/entries/icons/original/000/009/803/spongebob-squarepants-patrick-spongebob-patrick-star-background-225039.jpg" alt="Patrick">
+                </v-list-tile-avatar>
+    
+                <v-list-tile-content>
+                    <v-list-tile-title>Patrick Star</v-list-tile-title>
+                    <v-list-tile-sub-title>Founder of the Big Rock Club</v-list-tile-sub-title>
+                </v-list-tile-content>
+                </v-list-tile>
+
+                <v-spacer></v-spacer>
+                <v-subheader inset>Navigation</v-subheader>
+                <v-divider inset></v-divider>
+
+                <v-list-tile-content style="padding-left: 10px;">
+                    <v-list-tile-title>Link 1</v-list-tile-title>
+                    <v-list-tile-sub-title>This will be a link</v-list-tile-sub-title>
+                </v-list-tile-content>
+
+                <v-list-tile-content style="padding-left: 10px;">
+                    <v-list-tile-title>Link 2</v-list-tile-title>
+                    <v-list-tile-sub-title>This, too, will be a link</v-list-tile-sub-title>
+                </v-list-tile-content>
+
+                <v-list-tile-content style="padding-left: 10px;">
+                    <v-list-tile-title>Link 3</v-list-tile-title>
+                    <v-list-tile-sub-title>And this also</v-list-tile-sub-title>
+                </v-list-tile-content>
+            </v-list>
+  
+          <v-divider></v-divider>
+  
+          <v-card-actions>
+            <v-spacer></v-spacer>
+          </v-card-actions>
+        </v-card>
+        </v-menu>
+      <v-toolbar-title>YCAS Radio Telescope</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn to="/home">Home</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+        <v-container>\
+            <v-list-tile >
+                <v-list-tile-content class="white--text">
+                    <v-list-tile-title>Start Date:</v-list-tile-title>
+                    <v-list-tile-sub-title class = "pl-3">{{ startMonth }}/{{ startDay }}/{{ startYear }} {{ startHour }}:{{ startMinute }} {{ period }}</v-list-tile-sub-title>
+                </v-list-tile-content>
+                <v-list-tile-action>
+                    <v-btn icon ripple>
+                        <v-icon>edit</v-icon>
+                    </v-btn>
+                </v-list-tile-action>
+            </v-list-tile>
+            <v-divider></v-divider>
+            <v-list-tile>
+                <v-list-tile-content class="white--text">
+                    <v-list-tile-title >End Date:</v-list-tile-title>
+                    <v-list-tile-sub-title class = "pl-3">{{ endMonth }}/{{ endDay }}/{{ endYear }} {{ endHour }}:{{ endMinute }} {{ endPeriod }}</v-list-tile-sub-title>
+                </v-list-tile-content>
+                <v-list-tile-action>
+                    <v-btn icon ripple>
+                        <v-icon>edit</v-icon>
+                    </v-btn>
+                </v-list-tile-action>
+            </v-list-tile>
+            <v-divider></v-divider>
+            <v-list-tile>
+                <v-list-tile-content class="white--text">
+                    <v-list-tile-title>Privacy:</v-list-tile-title>
+                    <v-list-tile-sub-title class = "pl-3">{{ privacy }}</v-list-tile-sub-title>
+                </v-list-tile-content>
+                <v-list-tile-action>
+                    <v-btn icon ripple>
+                        <v-icon>edit</v-icon>
+                    </v-btn>
+                </v-list-tile-action>
+            </v-list-tile>
+            <v-divider></v-divider>
+            <v-list-tile>
+                <v-list-tile-content class="white--text">
+                    <v-list-tile-title>Celestial Body:</v-list-tile-title>
+                    <v-list-tile-sub-title class = "pl-3">{{ celestialBody }}</v-list-tile-sub-title>
+                </v-list-tile-content>
+                <v-list-tile-action>
+                    <v-btn icon ripple>
+                        <v-icon>edit</v-icon>
+                    </v-btn>
+                </v-list-tile-action>
+            </v-list-tile>
+            <v-divider></v-divider>
+            <v-list-tile>
+                <v-list-tile-content class="white--text">
+                    <v-list-tile-title>Telescope:
+                    </v-list-tile-title>
+                    <v-list-tile-sub-title class = "pl-3">{{ Tele }}</v-list-tile-sub-title>
+                </v-list-tile-content>
+                <v-list-tile-action>
+                    <v-btn icon ripple>
+                        <v-icon>edit</v-icon>
+                    </v-btn>
+                </v-list-tile-action>
+            </v-list-tile>
+            <v-divider></v-divider>
+            
+            
+        
+    </v-container>
+    </div>
+    
+</template>
+<script>
+
+export default {
+    name: "AppointmentView",
+    data() {
+            return {
+
+                startDay: '6',
+                startMonth: '10',
+                startYear: '2018',
+                endDay: '6', endMonth: '10', endYear: '2018',
+                startHour: '6', startMinute: '30', period: 'am',
+                endHour: '1', endMinute: '45', endPeriod: 'pm',
+                privacy: 'private',
+                celestialBody: 'Mars',
+                Tele: 1,
+
+                items: [
+                { title: 'Start Date', day: "6", month: "10", year: "2018", h: "6", mm: "30", period: "am"},
+                { title: 'End Date', day: "6", month: "10", year: "2018", h: "1", mm: "45", period: "pm"},
+                { title: 'Privacy', private:"true"},
+                { title: 'Coordinates', celestialBody: "Mars"},
+                { title: 'Telescope', enum: '1'}
+                ],
+        }
+    }
+}
+</script>
+
+<style scoped>
+    
+</style>
