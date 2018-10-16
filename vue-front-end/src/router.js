b91ce1727950560704b2a4ad7387e3dc13a3af89
Joey Hopkins <jhopkins1@ycp.edu>
2018-10-16 17:30:20 -0400

    implemented view profile page

--- vue-front-end/src/router.js
+++ vue-front-end/src/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Login from './views/Login.vue'
 import Register from './views/Register.vue'
+import viewProfile from './views/viewProfile.vue'
 
 Vue.use(Router)
 
@@ -24,6 +25,11 @@ const router = new Router({
       path: '/register',
       name: 'register',
       component: Register
+    },
+    {
+      path: '/viewProfile',
+      name: 'viewProfile',
+      component: viewProfile
     }
   ]
 })
