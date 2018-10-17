d906678f995f5bc65b9ed444d6b4540e40635974
Derek McClellan <dmcclellan@ycp.edu>
2018-10-16 21:58:55 -0400

    Add confirmation modal to edit profile page

--- vue-front-end/src/views/EditProfile.vue
+++ vue-front-end/src/views/EditProfile.vue
@@ -47,13 +47,25 @@
                         </v-flex>
                     </v-container>
                     <v-card-actions>
-                        <v-btn @click="cancelEdit">Cancel</v-btn>
+                        <v-btn color="red darken-1" @click="cancelEdit">Cancel</v-btn>
                         <v-spacer></v-spacer>
                         <v-btn color="primary" type="submit">Save</v-btn>
                     </v-card-actions>
                 </v-form>
             </v-card>
         </v-container>
+        <v-dialog max-width="250px" dark v-model="confirmModal" persistent>
+                <v-card>
+                    <v-card-title class="headline">Are you sure you wish to cancel?</v-card-title>
+                    <v-card-text>If you cancel, you will lose all progress on this form and will have to fill it out again.</v-card-text>
+                    <v-spacer></v-spacer>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="red darken-1" @click="confirmModal = false">No</v-btn>
+                        <v-btn color="green darken-1" @click="homeRedirect">Yes</v-btn>
+                    </v-card-actions>
+                </v-card>
+        </v-dialog>
     </v-app>
 </template>
 
@@ -72,10 +84,14 @@ export default {
                 type: "Student"
             },
             types: ['Guest', 'Member', 'Student', 'Researcher'],
+            confirmModal: false
         }
     },
     methods: {
         cancelEdit() {
+            this.confirmModal = true
+        },
+        homeRedirect() {
             router.go(-1);
         }
     }
