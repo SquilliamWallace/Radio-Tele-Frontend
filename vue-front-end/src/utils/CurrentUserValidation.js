dad29648d5f16ccfe5cbcb76de88d13d11baec9b
Derek McClellan <dmcclellan@ycp.edu>
2018-10-30 19:54:33 -0400

    Change validation to throw user back one page instead of back to home

--- vue-front-end/src/utils/CurrentUserValidation.js
+++ vue-front-end/src/utils/CurrentUserValidation.js
@@ -2,7 +2,7 @@ import router from '../router'
 export default{
     validateCurrentUser: function(store){
         if(store.state.currentUserId){
-            router.push('/home')
+            router.go(-1);
         }
         else{
             router.push('/')
