1a41e15b39cf0adf09db9d4ba36ee8919a9e280a
Derek McClellan <dmcclellan@ycp.edu>
2018-10-31 12:50:19 -0400

    Add navbar to under construction

--- vue-front-end/src/views/UnderConstruction.vue
+++ vue-front-end/src/views/UnderConstruction.vue
@@ -1,21 +1,30 @@
 <template>
     <v-app>
+        <navigation-bar></navigation-bar>
         <h1 class="text-style">This page is currently under construction</h1>
         <h3 class="text-style">Please be patient while we implement the necessary features</h3>
-        <img src=https://ak8.picdn.net/shutterstock/videos/13288688/thumb/1.jpg>
+        <img class="image-style" src=https://ak8.picdn.net/shutterstock/videos/13288688/thumb/1.jpg>
     </v-app>
 </template>
 
 <script>
+import NavigationBar from "../components/NavigationBar.vue";
 export default {
     name: "UnderConstruction",
-
+    components: {
+        NavigationBar
+    }
 }
 </script>
 
 <style scoped>
 .text-style{
-    color: bisque;
+    color: #f0ead6;
+}
+.image-style{
+    padding-top: 50px;
+    padding-left: 200px;
+    padding-right: 200px;
 }
 </style>
 
