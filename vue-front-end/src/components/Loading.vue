785ba7d9e8ece66f729be70df5b2ae592b55c310
Derek McClellan <dmcclellan@ycp.edu>
2018-11-04 15:14:12 -0500

    Add loading component and store mutation

---
+++ vue-front-end/src/components/Loading.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <v-progress-circular
+            indeterminate
+            color="primary"
+        ></v-progress-circular>
+        <div>Loading...</div>
+    </div>    
+</template>
+
+<script>
+export default {
+    name: "Loading"
+}
+</script>
+
+<style scoped>
+
+</style>
