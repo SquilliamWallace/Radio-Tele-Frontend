c71cc8908db25cd13e56c88e9eb8a67a56b04a79
mhasz239 <mhasz239@gmail.com>
2018-11-08 12:31:58 -0500

    Changed text to private event modal

--- vue-front-end/src/components/PrivateEvent.vue
+++ vue-front-end/src/components/PrivateEvent.vue
@@ -2,7 +2,7 @@
     <v-dialog class="alert-modal" width="50%" dark :value="value" persistent>
         <v-card>
             <v-card-title class="headline">This is a private event.</v-card-title>
-            <v-card-text>You are not the event owner. The owner of this event has selected to make this private. Please select a public event that is blue, or select your own event.</v-card-text>
+            <v-card-text>The owner of this event has selected to make this private. Please select a public event that is blue, or select your own event.</v-card-text>
             <v-spacer></v-spacer>
             <v-card-actions>
                 <v-spacer></v-spacer>
