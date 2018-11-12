77fdcb22d57e8836ec1827d28aaa6a713a71123d
Derek McClellan <dmcclellan@ycp.edu>
2018-11-12 13:28:14 -0500

    Fix ssl protocol error

--- vue-front-end/src/ApiDriver.js
+++ vue-front-end/src/ApiDriver.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 import Headers from './utils/Headers';
 
-let baseUrl = "https://ec2-52-90-251-86.compute-1.amazonaws.com:8080/api/";
+let baseUrl = "https://prod-api.ycpradiotelescope.com:8080/api/";
 export default {
     //API endpoints go here
     User: {
