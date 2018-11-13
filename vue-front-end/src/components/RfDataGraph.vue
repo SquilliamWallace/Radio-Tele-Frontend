ad20dcd2bce2dd9c92817dd1e0c9925ba2a14d36
Derek McClellan <dmcclellan@ycp.edu>
2018-11-12 20:27:52 -0500

    Implement chart.js and rf data chart

---
+++ vue-front-end/src/components/RfDataGraph.vue
@@ -0,0 +1,48 @@
+<script>
+import { Line } from 'vue-chartjs'
+
+export default {
+  extends: Line,
+  props: ['chartdata', 'styles'],
+  data() {
+      return {
+          datacollection: {
+            labels: ['January', 'February', 'March', 'April'],
+            datasets: [
+                {
+                    label: 'Radio Frequency Intensity Over Time',
+                    backgroundColor: '#0c03b2',
+                    data: [4, 2, 5, 3.5]
+                }
+            ]
+      },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            scales: {
+                xAxes: [{
+                    display: true,
+                    scaleLabel: {
+                    display: true,
+                    labelString: 'Time of Capture'
+                    }
+                }],
+                yAxes: [{
+                    display: true,
+                    scaleLabel: {
+                    display: true,
+                    labelString: 'Signal Intensity'
+                    }
+                }]
+            }
+        }
+    }
+  },
+  mounted () {
+    this.renderChart(this.chartdata, this.options)
+  }
+}
+</script>
+
+<style scoped>
+</style>
