a713f5eb831455c8ea4a3bfa57169dae427dc1b6
cody <cspath1@ycp.edu>
2018-11-12 21:28:16 -0500

    Add back-end error handling to editing an appointment

--- vue-front-end/src/components/EditAppointment.vue
+++ vue-front-end/src/components/EditAppointment.vue
@@ -8,6 +8,10 @@
                     <v-flex xs12 sm6>
                         <v-text-field
                         v-model="appointmentObj.start.value"
+                        :rules="[rules.dateRequired]"
+                        :error=appointmentObj.start.hasError
+                        :error-messages=appointmentObj.start.errorMessage
+                        :valiate-on-blur=true
                         color="blue darken-2"
                         label="Start Time"
                         required
@@ -16,6 +20,9 @@
                     <v-flex xs12 sm6>
                         <v-text-field
                         v-model="appointmentObj.end.value"
+                        :rules="[rules.dateRequired]"
+                        :error=appointmentObj.end.hasError
+                        :error-messages=appointmentObj.end.errorMessage
                         color="blue darken-2"
                         label="End Time"
                         required
@@ -24,16 +31,24 @@
                     <v-flex xs12 sm6>
                         <v-text-field
                         v-model="appointmentObj.rightAscension.value"
+                        :rules="[rules.numRequired]"
+                        :error=appointmentObj.rightAscension.hasError
+                        :error-messages=appointmentObj.rightAscension.errorMessage
                         color="blue darken-2"
                         label="Right Ascension"
+                        type="number"
                         required
                         ></v-text-field>
                     </v-flex>
                     <v-flex xs12 sm6>
                         <v-text-field
                         v-model="appointmentObj.declination.value"
+                        :rules="[rules.numRequired]"
+                        :error=appointmentObj.declination.hasError
+                        :error-messages=appointmentObj.declination.errorMessage
                         color="blue darken-2"
                         label="Declination"
+                        type="number"
                         required
                         ></v-text-field>
                     </v-flex>
@@ -51,6 +66,7 @@
                     <v-btn flat @click="$emit('input')">Cancel</v-btn>
                     <v-spacer></v-spacer>
                     <v-btn
+                    :disabled="!validRequest"
                     flat
                     color="primary"
                     type="submit"
@@ -66,13 +82,23 @@ import Event from '../main.js'
 import ApiDriver from '../ApiDriver'
 import HttpResponse from '../utils/HttpResponse'
 import CurrentUserValidation from '../utils/CurrentUserValidation'
+import CustomErrorHandler from '../utils/CustomErrorHandler.js';
 export default {
+    data() {
+        return {
+            rules: {
+                dateRequired: val => (val && val.length > 0) || 'Required field',
+                numRequired: val => (val && val.toString.length > 0) || 'Required field'
+            }
+        }
+    },
     props: {
         appointmentObj: {},
         value: false
     },
     methods: {
         submit() {
+            this.clearErrors();
 
             let data = JSON.stringify({
                 startTime: new Date(this.appointmentObj.start.value).toUTCString(),
@@ -83,34 +109,56 @@ export default {
                 declination: this.appointmentObj.declination.value
             });
 
-            // This will need changed to properly handle success or failure scenarios
             ApiDriver.Appointment.update(this.appointmentObj.id.value, data).then((response) => {
-                console.log(response);
                 HttpResponse.then(response, (data) => {
                     this.$emit('edited', this.appointmentObj)
                     this.$emit('input');
 
                     }, (status, errors) => {
                         if (parseInt(status) === 403) {
-                            this.$swal({
-                            title: '<span style="color:#f0ead6">Error!<span>',
-                            html: '<span style="color:#f0ead6">Access Denied<span>',
-                            type: 'error',
-                            background: '#302f2f'
-                        });
-                            CurrentUserValidation.validateCurrentUser(this.$store);
+                            HttpResponse.accessDenied(this)
+                        } else if (parseInt(status) === 404) {
+                            HttpResponse.notFound(this, errors)
                         } else {
-                            console.log(status)
-                            console.log(errors)
+                            this.handleErrors(errors);
                         }
-                    })
+                    });
             });
-            
+        },
+        handleErrors(errors) {
+            for (var field in errors) {
+                let message = errors[field][0];
+
+                if (field === "RIGHT_ASCENSION") {
+                    CustomErrorHandler.populateError(this.appointmentObj.rightAscension, message)
+                } else if (field === "DECLINATION") {
+                    CustomErrorHandler.populateError(this.appointmentObj.declination, message)
+                } else if (field === "START_TIME") {
+                    CustomErrorHandler.populateError(this.appointmentObj.start, message)
+                } else if (field === "END_TIME") {
+                    CustomErrorHandler.populateError(this.appointmentObj.end, message)
+                } else {
+                    HttpResponse.generalError(this, message, false)
+                }
+            }
+        },
+        clearErrors() {
+            CustomErrorHandler.clearError(this.appointmentObj.rightAscension)
+            CustomErrorHandler.clearError(this.appointmentObj.declination)
+            CustomErrorHandler.clearError(this.appointmentObj.start)
+            CustomErrorHandler.clearError(this.appointmentObj.end)
+        }
+    },
+    computed: {
+        validRequest() {
+            return (this.appointmentObj.rightAscension.value &&
+                    this.appointmentObj.declination.value &&
+                    this.appointmentObj.start.value &&
+                    this.appointmentObj.end.value)
         }
     }
 }
 </script>
-
 <style scoped>
 
 </style>
