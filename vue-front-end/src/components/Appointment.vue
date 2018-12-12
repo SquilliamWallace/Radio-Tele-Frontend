2fd833113b1982f41374530e9e3ca29102138941
Derek McClellan <dmcclellan@ycp.edu>
2018-12-12 14:05:17 -0500

    Add missing rule

--- vue-front-end/src/components/Appointment.vue
+++ vue-front-end/src/components/Appointment.vue
@@ -207,7 +207,8 @@ export default {
                 dateRequired: val => (val && val.length > 0) || 'Required field',
                 rightAscHours: val => (val && val.toString().length > 0 && val < 24 && val >= 0) || 'Must be between 0 and 23 hours',
                 rightAscMinutes: val => (val && val.toString().length > 0 && val < 60 && val >= 0) || 'Must be between 0 and 59 minutes',
-                rightAscSeconds: val => (val && val.toString().length > 0 && val < 60 && val >= 0) || 'Must be between 0 and 59 seconds'
+                rightAscSeconds: val => (val && val.toString().length > 0 && val < 60 && val >= 0) || 'Must be between 0 and 59 seconds',
+                numRequired: val => (val && val.toString().length > 0)
             },
             snackbar: false,
         }
