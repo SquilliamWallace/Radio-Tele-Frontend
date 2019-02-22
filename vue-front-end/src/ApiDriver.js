e9cd656081040f6b0f907228c44562695ac3ea31
Derek McClellan <dmcclellan@ycp.edu>
2019-02-22 12:04:49 -0500

    Fix ApiDriver again

--- vue-front-end/src/ApiDriver.js
+++ vue-front-end/src/ApiDriver.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 import Headers from './utils/Headers';
 
-let baseUrl = "https://prod-api.ycpradiotelescope.com:8080/api/";
+let baseUrl = "https://prod-api.ycpradiotelescope.com/api/";
 export default {
     //API endpoints go here
     User: {
@@ -113,7 +113,7 @@ export default {
     },
 
     login: function(data) {
-      return axios.post("http://api.ycpradiotelescope.com:8080/login?email=" + data.username.value + "&password=" + data.password.value, JSON.stringify(data))
+      return axios.post("https://prod-api.ycpradiotelescope.com/login?email=" + data.username.value + "&password=" + data.password.value, JSON.stringify(data))
     },
     logout: function () {
       return axios.post(baseUrl + "logout", {}, Headers.retrieveHeaders())
