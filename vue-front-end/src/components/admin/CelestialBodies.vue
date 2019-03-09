94c46d24fa40062572e5098fa0ec7b4b46b84a97
Joey Hopkins <jhopkins1@ycp.edu>
2019-03-09 14:25:54 -0500

    Added celestial bodies tab to admin page

---
+++ vue-front-end/src/components/admin/CelestialBodies.vue
@@ -0,0 +1,24 @@
+<template>
+    <p>YEEEET</p>
+</template>
+<script>
+import router from '../../router';
+import ApiDriver from '../../ApiDriver';
+import HttpResponse from '../../utils/HttpResponse';
+import CurrentUserValidation from  '../../utils/CurrentUserValidation';
+import Loading from "../../components/utility/Loading"
+export default {
+    name: 'CelestialBodies',
+    data(){
+        return{
+            data: {
+               
+            },
+           
+        }
+    },
+}
+</script>
+<style scoped>
+
+</style>
