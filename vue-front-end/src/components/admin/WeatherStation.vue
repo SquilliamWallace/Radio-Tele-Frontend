a290a246dc6c32733a6708c124f5d00a8aefe14d
Nolan Hamilton <nhamilton@ycp.edu>
2019-10-14 21:32:05 -0400

    Created WeatherStation.vue page.

---
+++ vue-front-end/src/components/admin/WeatherStation.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+    <loading v-if="$store.state.isLoading"></loading>
+    <v-card v-if="!$store.state.isLoading" width = "100%">
+
+    </v-card>
+</div>
+</template>
+<script>
+import router from '../../router';
+import ApiDriver from '../../ApiDriver';
+import HttpResponse from '../../utils/HttpResponse';
+import CurrentUserValidation from  '../../utils/CurrentUserValidation';
+import Loading from "../../components/utility/Loading"
+
+export default {
+    name: 'WeatherStation',
+    data(){
+
+    },
+    methods:{
+
+    },
+    mounted: function(){
+        
+    },
+    components: {
+        Loading,
+    }
+};
+
+</script>
+<style scoped>
+    
+</style>
