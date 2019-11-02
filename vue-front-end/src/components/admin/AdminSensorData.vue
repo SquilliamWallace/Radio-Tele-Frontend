ccda8f4cc21fc73b5674003609067e126747b480
Nolan Hamilton <nhamilton@ycp.edu>
2019-11-02 18:31:58 -0400

    Sensor Page: Implemented skeleton layout.

--- vue-front-end/src/components/admin/AdminSensorData.vue
+++ vue-front-end/src/components/admin/AdminSensorData.vue
@@ -4,7 +4,55 @@
     <v-card v-if="!$store.state.isLoading" width = "100%">
           <!-- Insert code here -->
           <h1>Sensor Data</h1>
-    </v-card>
+
+<v-container grid text-xs-center>
+    <v-layout row>
+        <v-flex md4>
+                <v-card dark >
+                    <v-card-text class="text--primary">Text Here</v-card-text>
+                </v-card>
+        </v-flex>
+
+        <v-flex md4>
+                <v-card dark color="green">
+                    <v-card-text class="px-5">OK</v-card-text>
+                </v-card>
+        </v-flex>
+
+        <v-flex md>
+            <v-card-actions class="justify-start">
+                <div>
+                    <v-switch class="ma-0" inset label="Override" background-color="transparent" color="blue darken-5"></v-switch>
+                </div>
+            </v-card-actions>
+        </v-flex>
+    </v-layout>
+
+    <v-layout row>
+        <v-flex md4>
+                <v-card dark >
+                    <v-card-text>Text Here</v-card-text>
+                </v-card>
+        </v-flex>
+
+        <v-flex md4>
+                <v-card dark color="green">
+                    <v-card-text class="px-5">OK</v-card-text>
+                </v-card>
+        </v-flex>
+
+        <v-flex md>
+            <v-card-actions class="justify-start">
+                <div>
+                    <v-switch class="ma-0" inset label="Override" background-color="transparent" color="blue darken-5"></v-switch>
+                </div>
+            </v-card-actions>
+        </v-flex>
+    </v-layout>
+
+</v-container>
+</v-card>
+
 </div>
 </template>
 <script>
@@ -15,7 +63,7 @@ import CurrentUserValidation from  '../../utils/CurrentUserValidation';
 import Loading from "../../components/utility/Loading"
 import moment from 'moment';    // Is this page going to be working with dates. 
 export default {
-    name: 'Sensor Data',
+    name: 'SensorData',
     data(){
         return {
             // insert data
