cad7c25b6a1bdfa8fef797205792addef2daa388
Nolan Hamilton <nhamilton@ycp.edu>
2019-11-04 18:00:49 -0500

    Sensor Page: Added dataset and variables.

--- vue-front-end/src/components/admin/AdminSensorData.vue
+++ vue-front-end/src/components/admin/AdminSensorData.vue
@@ -36,8 +36,8 @@
         </v-flex>
 
         <v-flex md4>
-                <v-card dark color="yellow">
-                    <div class="sensor-status">Warning</div>
+                <v-card dark :color="gateStatColor">
+                    <div class="sensor-status">{{ gateStatText }}</div>
                 </v-card>
         </v-flex>
 
@@ -58,8 +58,8 @@
         </v-flex>
 
         <v-flex md4>
-                <v-card dark color="green">
-                    <div class="sensor-status">OK</div>
+                <v-card dark :color="proximityStatColor">
+                    <div class="sensor-status">{{ proximityStatText }}</div>
                 </v-card>
         </v-flex>
 
@@ -80,8 +80,52 @@
         </v-flex>
 
         <v-flex md4>
-                <v-card dark color="red">
-                    <div class="sensor-status">Error</div>
+                <v-card dark :color="azimuthStatColor">
+                    <div class="sensor-status">{{ azimuthStatText }}</div>
+                </v-card>
+        </v-flex>
+
+        <v-flex md>
+            <v-card-actions class="justify-start">
+                <div>
+                    <v-switch class="ma-0" inset label="Override" background-color="transparent" color="blue darken-5"></v-switch>
+                </div>
+            </v-card-actions>
+        </v-flex>
+    </v-layout>
+
+    <v-layout row>
+        <v-flex md4>
+                <v-card dark >
+                        <div class="sensor-name">Elevation Motor</div>
+                </v-card>
+        </v-flex>
+
+        <v-flex md4>
+                <v-card dark :color="elevationStatColor">
+                    <div class="sensor-status">{{ elevationStatText }}</div>
+                </v-card>
+        </v-flex>
+
+        <v-flex md>
+            <v-card-actions class="justify-start">
+                <div>
+                    <v-switch class="ma-0" inset label="Override" background-color="transparent" color="blue darken-5"></v-switch>
+                </div>
+            </v-card-actions>
+        </v-flex>
+    </v-layout>
+
+    <v-layout row>
+        <v-flex md4>
+                <v-card dark >
+                        <div class="sensor-name">Weather Station</div>
+                </v-card>
+        </v-flex>
+
+        <v-flex md4>
+                <v-card dark :color="weatherStatColor">
+                    <div class="sensor-status">{{ weatherStatText }}</div>
                 </v-card>
         </v-flex>
 
@@ -110,7 +154,36 @@ export default {
     name: 'SensorData',
     data(){
         return {
-            // insert data
+            // Overall data
+
+            // status values
+            gateStatus: 0,
+            proximityStatus: 0,
+            azimuthStatus: 0, 
+            elevationStatus: 0, 
+            weatherStatus: 0, 
+
+            // status colors
+            gateStatColor: "green",
+            proximityStatColor: "yellow",
+            azimuthStatColor: "red",
+            elevationStatColor: "green",
+            weatherStatColor: "red",
+            // status text
+            gateStatText: "OK",
+            proximityStatText: "WARNING",
+            azimuthStatText: "ERROR", 
+            elevationStatText: "OK",
+            weatherStatText: "ERROR",
+
+            // imported data
+            dbData: [
+                {   id: 1, name: "Gate", dataType: "tinyint", description: "Resembles status of gate sensor", value: 0 },
+                {   id: 2, name: "Proximity", dataType: "tinyint", description: "Resembles status of proximity sensor", value: 1 },
+                {   id: 3, name: "Azimuth Motor", dataType: "tinyint", description: "Resembles status of azimuth motor", value: 2 },
+                {   id: 4, name: "Elevation Motor", dataType: "tinyint", description: "Resembles status of elevation motor", value: 0 },
+                {   id: 5, name: "Weather Station", dataType: "tinyint", description: "Resembles status of weather station", value: 1 }
+            ]
         }
     },
     methods:{
