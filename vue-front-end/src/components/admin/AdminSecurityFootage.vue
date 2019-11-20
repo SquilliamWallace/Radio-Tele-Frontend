4e4a72ba1e3d0718c0aeec309895220a0ac8ad14
Nolan Hamilton <nhamilton@ycp.edu>
2019-11-20 10:57:12 -0500

    Security Page: Added Security Footage Page.

---
+++ vue-front-end/src/components/admin/AdminSecurityFootage.vue
@@ -0,0 +1,42 @@
+<template>
+<div>
+    <loading v-if="$store.state.isLoading"></loading>
+    <v-card v-if="!$store.state.isLoading" width = "100%">
+        <h1>Security Footage</h1>
+
+        
+        
+    </v-card>
+</div>
+</template>
+<script>
+import router from '../../router';
+import ApiDriver from '../../ApiDriver';
+import HttpResponse from '../../utils/HttpResponse';
+import CurrentUserValidation from  '../../utils/CurrentUserValidation';
+import Loading from "../../components/utility/Loading";
+import moment from 'moment';    // Is this page going to be working with dates. 
+export default {
+    name: 'SecurityFootage',
+    data(){
+        return {
+            
+        }
+    },
+    methods:{
+        
+    },
+    mounted: function(){
+        
+    },
+    components: {
+        Loading
+    },
+    computed: {
+        
+    }
+};
+</script>
+<style scoped>
+
+</style>
--- vue-front-end/src/views/Administration.vue
+++ vue-front-end/src/views/Administration.vue
@@ -30,6 +30,10 @@
           Celestial Body Management
           <v-icon>public</v-icon>
         </v-tab>
+        <v-tab>
+          Security Footage
+          <v-icon>public</v-icon>
+        </v-tab>
         
         <v-tab-item>
           <admin-log></admin-log>
@@ -50,6 +54,10 @@
         <v-tab-item>
           <celestial-bodies></celestial-bodies>
         </v-tab-item>
+
+        <v-tab-item>
+          <admin-security-footage></admin-security-footage>
+        </v-tab-item>
       </v-tabs>
 
   </div>
@@ -65,6 +73,7 @@ import CurrentUserValidation from '../utils/CurrentUserValidation';
 import AdminUserApproval from '../components/admin/AdminUserApproval.vue';
 import AdminAppointmentApproval from '../components/admin/AdminAppointmentApproval.vue';
 import CelestialBodies from '../components/admin/CelestialBodies.vue';
+import AdminSecurityFootage from '../components/admin/AdminSecurityFootage.vue';
 import Loading from "../components/utility/Loading"
  export default {
     title: "Radio Telescope 1.1.0",
@@ -97,7 +106,8 @@ import Loading from "../components/utility/Loading"
         AdminUserApproval,
         Loading,
         AdminAppointmentApproval,
-        CelestialBodies
+        CelestialBodies,
+        AdminSecurityFootage
     },
     mounted() {
       this.authenticate()
