7a7ef406c8bb929d75bb5ce6364da4c6cd43e08e
Dakota Hilbert <dhilbert1@ycp.edu>
2020-03-30 20:24:04 -0400

    .

--- vue-front-end/src/components/admin/AdminSensorData.vue
+++ vue-front-end/src/components/admin/AdminSensorData.vue
@@ -83,7 +83,7 @@ export default {
             overallStatColor: "green",
             overallStatText: "OK",
 
-             thresholds: [],
+            thresholds: [],
 
             sensorList: [
                 'gate',
