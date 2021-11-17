86cbfeafc3b43b3e5832c610b1c5f8b9af56f270
Lucas Gartrell <lplaudsmith@ycp.edu>
2021-11-17 10:41:48 -0800

    Admin Sensor Initialization Page

--- vue-front-end/src/components/admin/AdminSensorInitialization.vue
+++ vue-front-end/src/components/admin/AdminSensorInitialization.vue
@@ -5,16 +5,24 @@
     <v-container grid text-xs-center>
         <v-card-title primary-title class="justify-center">Sensor Network Sensor Initialization</v-card-title>
     </v-container>
-    <v-checkbox v-model="SensorInitData" label="Elevation Encoder" value="1"></v-checkbox>
-    <v-checkbox v-model="SensorInitData" label="Azimuth Encoder" value="2"></v-checkbox>
-    <v-checkbox v-model="SensorInitData" label="Elevation Temperature" value="3"></v-checkbox>
-    <v-checkbox v-model="SensorInitData" label="Azimuth Temperature" value="4"></v-checkbox>
-    <v-checkbox v-model="SensorInitData" label="Azimuth Accelerometer" value="5"></v-checkbox>
-    <v-checkbox v-model="SensorInitData" label="Elevation Accelerometer" value="6"></v-checkbox>
-    <v-checkbox v-model="SensorInitData" label="Counterbalance Accelerometer" value="7"></v-checkbox>
-    <v-text-field v-model="DataRetTimeout" label="Data Retrieval Timeout (seconds)" outlined></v-text-field>
-    <v-text-field v-model="InitTimeout" label="Initialization Timeout (seconds)" outlined></v-text-field>
-    <v-btn @click="webSocketTest">Submit</v-btn>
+    <v-container fluid>
+        <v-checkbox v-model="sensorInit.AzimuthTemperature" label="Azimuth Temperature"></v-checkbox>
+        <v-checkbox v-model="sensorInit.ElevationTemperature" label="Elevation Temperature"></v-checkbox>
+        <v-checkbox v-model="sensorInit.AzimuthAccelerometer" label="Azimuth Accelerometer"></v-checkbox>
+        <v-checkbox v-model="sensorInit.ElevationAccelerometer" label="Elevation Accelerometer"></v-checkbox>
+        <v-checkbox v-model="sensorInit.CounterbalanceAccelerometer" label="Counterbalance Accelerometer"></v-checkbox>
+        <v-checkbox v-model="sensorInit.AzimuthEncoder" label="Azimuth Encoder"></v-checkbox>
+        <v-checkbox v-model="sensorInit.ElevationEncoder" label="Elevation Encoder"></v-checkbox>
+    </v-container>
+    <v-container>
+        <input v-model="sensorInit.InitTimeout" placeholder="Initialization Timeout (seconds)" name="InitTimeout">
+    </v-container>
+    <v-container>
+        <input v-model="sensorInit.RetTimeOut" placeholder="Data Retrieval Timeout (seconds)" name="DataRetTimeout">
+    </v-container>
+
+    <v-btn @click="fillSensorInitData(sensorInit)">TEST WEBSOCKET BUTTON</v-btn>
+
 </v-card>
 
 </div>
@@ -29,14 +37,60 @@ export default {
     name: 'SensorInit',
     data () {
     return {
-        SensorInitData: [],
-        DataRetTimeOut: null,
-        InitTimeout: null,
+        Command: "SCRIPT: STOW",
+        sensorInit: {
+            ElevationEncoder: false,
+            AzimuthEncoder: false,
+            ElevationTemperature: false,
+            AzimuthTemperature: false,
+            AzimuthAccelerometer: false,
+            ElevationAccelerometer: false,
+            CounterbalanceAccelerometer: false,
+            RetTimeOut: null,
+            InitTimeout: null,
+        }
     }
   },
     methods:{
-        webSocketTest(){
-            ApiDriver.webSocket();
+        webSocketTest(command){
+            ApiDriver.webSocket(command);
+        },
+        fillSensorInitData(sensorInit){
+            const SensorInitData = [];
+            for(var i = 0; i < 9; i++){
+                SensorInitData[i] = 0;
+            }
+
+            if(sensorInit.AzimuthTemperature){
+                SensorInitData[0] = 1;
+            }
+            if(sensorInit.ElevationTemperature){
+                SensorInitData[1] = 1;
+            }
+            if(sensorInit.AzimuthAccelerometer){
+                SensorInitData[2] = 1;
+            }
+            if(sensorInit.ElevationAccelerometer){
+                SensorInitData[3] = 1;
+            }
+            if(sensorInit.CounterbalanceAccelerometer){
+                SensorInitData[4] = 1;
+            }
+            if(sensorInit.AzimuthEncoder){
+                SensorInitData[5] = 1;
+            }
+            if(sensorInit.ElevationEncoder){
+                SensorInitData[6] = 1;
+            }
+            if(sensorInit.InitTimeout != null){
+                SensorInitData[7] = sensorInit.InitTimeout;
+            }
+            if(sensorInit.RetTimeOut != null){
+                SensorInitData[8] = sensorInit.RetTimeOut;
+            }
+
+            var InitCommand = "VERSION | SENSOR_INIT | " + SensorInitData.toString() + " | " + new Date().toUTCString;
+            this.webSocketTest(InitCommand);      
         }
   }
 };
